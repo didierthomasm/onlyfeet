540cfb5d0cb73a87416e88549e750bb2df266754
didierthomasm <didierthomas.m@gmail.com>
2023-11-10 15:47:57 -0600

    Refactor Footer component to include styled components for improved readability and maintainability.

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,15 +1,53 @@
 import styled from "styled-components";
 
-const MainFooter = styled.footer`
-  background-color: #f8f9fa;
-  padding: 1rem;
+const FooterContainer = styled.footer`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 20px;
+  background-color: #F8F8F8;
+  color: #333;
+  font-size: 14px;
+`;
+
+const FooterLinks = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 10px;
+  margin-bottom: 20px;
+
+  a {
+    color: #333;
+    text-decoration: none;
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+`;
+
+const FooterBottom = styled.div`
+  border-top: 1px solid #CCC;
+  padding-top: 20px;
   text-align: center;
 `;
 
 export function Footer() {
   return (
-    <MainFooter>
-      <p>© 2023 Only Feet</p>
-    </MainFooter>
-  )
+    <FooterContainer>
+      <FooterLinks>
+        <a href="/contact">Contact</a>
+        <a href="/help">Help</a>
+        <a href="/about">About</a>
+        <a href="/store">Store</a>
+        <a href="/terms-of-service">Terms of Service</a>
+        <a href="/privacy">Privacy</a>
+        <a href="/dmca">DMCA</a>
+      </FooterLinks>
+      <FooterBottom>
+        ©2023 OnlyFeet
+      </FooterBottom>
+    </FooterContainer>
+  );
 }
