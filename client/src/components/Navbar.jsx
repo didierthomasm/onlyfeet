fe3ca14bacbd52b92ef96dd012297e4fb528b1c5
didierthomasm <didierthomas.m@gmail.com>
2023-11-12 14:16:49 -0600

    Refactor Profile and Navbar components, add NavbarStyle.js, and update package.json.

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,40 +1,59 @@
-import { NavLink } from "react-router-dom";
-import styled from "styled-components";
+import {NavLink} from "react-router-dom";
 
-const Nav = styled.nav`
-  ul {
-    display: flex;
-    list-style: none;
-    padding: 0;
-  }
-  
-  li {
-    margin: 0 1rem;
-  }
-  
-  a {
-    text-decoration: none;
-    color: #000;
-  }
-  
-  a:hover {
-    color: #333;
-  }
-`
+// Styles
+import Nav from '../assets/style/Header-Navbar/NavbarStyle.js'
+
+// Icons
+import {Person, PersonFill, PiggyBank as Earn, PiggyBankFill as EarnFill} from '@styled-icons/bootstrap/';
+import {Home} from '@styled-icons/heroicons-outline/';
+import {Home as HomeFill} from '@styled-icons/heroicons-solid/Home'
+import {BookContacts as Contact} from '@styled-icons/fluentui-system-regular/';
+import {BookContacts as ContactFill} from '@styled-icons/fluentui-system-filled/';
+import {Payments as Payment} from '@styled-icons/material-outlined/';
+import {Payments as PaymentFill} from '@styled-icons/material-sharp/';
+
+const section = [{
+  name: 'Home',
+  icon: <Home className="inactive-icon" size={32}/>,
+  iconActive: <HomeFill className="active-icon" size={32}/>,
+  path: '/'
+}, {
+  name: 'Profile',
+  icon: <Person className="inactive-icon" size={32}/>,
+  iconActive: <PersonFill className="active-icon" size={32}/>,
+  path: '/profile'
+}, {
+  name: 'Pay',
+  icon: <Payment className="inactive-icon" size={32}/>,
+  iconActive: <PaymentFill className="active-icon" size={32}/>,
+  path: '/payment'
+}, {
+  name: 'Earn',
+  icon: <Earn className="inactive-icon" size={32}/>,
+  iconActive: <EarnFill className="active-icon" size={32}/>,
+  path: '/deposit'
+}, {
+  name: 'Contact',
+  icon: <Contact className="inactive-icon" size={32}/>,
+  iconActive: <ContactFill className="active-icon" size={32}/>,
+  path: '/contact'
+}];
 
 export function Navbar() {
   return (
     <Nav>
       <ul>
-        <li>
-          <NavLink to={'/'}>Home</NavLink>
-        </li>
-        <li>
-          <NavLink to={'/about'}>About</NavLink>
-        </li>
-        <li>
-          <NavLink to={'/contact'}>Contact</NavLink>
-        </li>
+        {section.map((item) => {
+          return (
+            <li>
+              <NavLink to={item.path} className={({isActive}) => (isActive ? 'active' : '')}>
+                {item.icon}
+                {item.iconActive}
+                <span>{item.name}</span>
+              </NavLink>
+            </li>
+          )
+        })}
       </ul>
     </Nav>
   )
