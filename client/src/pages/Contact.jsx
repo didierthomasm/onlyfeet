a7bfc6fcfa89b8cdfe9e88ea1abb380f4a0c5a9f
didierthomasm <didierthomas.m@gmail.com>
2023-11-14 11:31:17 -0600

    Add Contact, Pay, and Earn pages with styled components and an image.

---
+++ client/src/pages/Contact.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import image from "../assets/img/Errors/inProduction.png";
+
+const Main = styled.main`
+
+`;
+export function Contact() {
+  return (
+    <Main>
+
+      <img src={image} alt={''} style={{ height: '100vh'}}/>
+    </Main>
+  )
+}
