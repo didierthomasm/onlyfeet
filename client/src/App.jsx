f11c78e4123ad8028189d7ee0c43c06acce01a19
didierthomasm <didierthomas.m@gmail.com>
2023-11-14 11:33:22 -0600

    Refactored App.jsx and main.jsx to use styled-components and added new pages.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,22 @@
 import {Outlet, useLocation} from "react-router-dom";
 import {useState} from "react";
+import {GlobalProvider} from "./context/GlobalState.jsx";
+import styled from "styled-components";
 import GlobalStyle from "../src/assets/style/GlobalStyle"
 
 import {Footer} from "./components/Footer.jsx";
 import {LoginSignup} from "./pages/LoginSignup.jsx";
-import styled from "styled-components";
 import {Header} from "./components/Header.jsx";
 
 // Container for the entire layout
-const AppContainer = styled.div`
+const AppContainer = styled.main`
   display: grid;
   grid-template-columns: .5fr 2fr;
   justify-content: center;
   align-items: center;
   min-height: 100vh;
   max-width: 100vw;
+  margin: 0 auto;
 `;
 
 function App() {
@@ -26,17 +28,19 @@ function App() {
   return (
     <>
       <GlobalStyle/>
-      {isLoggedIn ? (
-        <AppContainer>
-          <Header />
-          <Outlet/>
-        </AppContainer>
-      ) : (
-        <>
-          <LoginSignup setIsLoggedIn={setIsLoggedIn} mode={isSignUpPage ? 'signup' : 'login'}/>
-          <Footer/>
-        </>
-      )}
+      <GlobalProvider>
+        {isLoggedIn ? (
+          <AppContainer>
+            <Header/>
+            <Outlet />
+          </AppContainer>
+        ) : (
+          <>
+            <LoginSignup setIsLoggedIn={setIsLoggedIn} mode={isSignUpPage ? 'signup' : 'login'}/>
+            <Footer/>
+          </>
+        )}
+      </GlobalProvider>
     </>
 
   );
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client'
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 
 import App from './App.jsx'
-import {Home} from "./pages/Home.jsx";
 import ErrorPage from "./pages/Error.jsx";
+import {Home} from "./pages/Home.jsx";
 import {LoginSignup} from "./pages/LoginSignup.jsx";
 import {Profile} from "./pages/Profile.jsx";
+import {Pay} from "./pages/Pay.jsx";
+import {Earn} from "./pages/Earn.jsx";
+import {Contact} from "./pages/Contact.jsx";
 
 const router = createBrowserRouter([
   {
@@ -29,7 +32,19 @@ const router = createBrowserRouter([
       {
         path: 'profile',
         element: <Profile />
-      }
+      },
+      {
+        path: 'payment',
+        element: <Pay />
+      },
+      {
+        path: 'earnings',
+        element: <Earn />
+      },
+      {
+        path: 'contact',
+        element: <Contact />
+      },
     ]
   }
 ])
