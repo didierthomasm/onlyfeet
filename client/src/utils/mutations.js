e3d636ca73f831d6dd2fb2fb62baeb366e425f12
Rodo Rivera <rudy_roddick@msn.com>
2023-11-16 21:04:28 -0600

    fix in server auth to bypass midleware; queries and mutations in client side

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -6,20 +6,49 @@ export const LOGIN_USER = gql`
       token
       user {
         _id
-        username
+        firstName
+        lastName
       }
     }
   }
 `;
 
 export const ADD_USER = gql`
-  mutation addUser($firstName: String!, $lastName: String!, $username: String!, $email: String!, $password: String!: $userRole: Number!) {
-    addUser(firstName: $firstName, lastName: $lastName, username: $username, email: $email, password: $password, userRole: $userRole) {
+  mutation addUser($firstName: String!, $lastName: String!, $email: String!, $password: String!: $role: Number!, $created_at: Date, $credits: Int) {
+    addUser(firstName: $firstName, lastName: $lastName, email: $email, password: $password, role: $userRole) {
       token
       user {
         _id
-        username
+        firstName
+        lastName
+        email
+        role
       }
     }
   }
 `;
+
+export const ADD_SUBSCRIPTION = gql`{
+  mutation addSubscription($follower: ID!, $creator: ID!, $startDate: Date, $endDate: Date, $isActive: Boolean, $subscriptionType: String){
+    addSubscription(follower: $follower, creator: $creator, startDate: $startDate, endDate: $endDate, isActive: $isActive, subscriptionType: $subscriptionType){
+      _id
+      startDate
+      endDate
+      subscriptionType
+    }
+  }
+}`;
+
+export const REMOVE_SUBSCRIPTION = gql`{
+  mutation removeSubscription($subscriptionId: ID){
+    removeSubscription(_id: subscriptionId){
+      _id
+      firstName
+      lastName
+      email
+      role
+      credits
+      subscribedTo
+    }
+  }
+}`;
