60c8e808bcb4aeeb56e23851a484f979f0abdc0b
Hola Bart <marcoferrusquia@gmail.com>
2023-11-21 13:43:24 -0600

    pequeño cambio en index para requerir video e img

--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,7 @@
 const User = require('./User');
 const Content = require('./Content');
 const Subscription = require('./Subscription');
+const Video = require('./Video')
+const Image = require ('./Image')
 
-module.exports = { User, Content, Subscription };
+module.exports = { User, Content, Subscription, Video, Image };
