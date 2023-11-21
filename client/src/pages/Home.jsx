966688594ffcf737308358d9528b965a85af2547
didierthomasm <didierthomas.m@gmail.com>
2023-11-21 11:20:26 -0600

    Refactor Home.jsx to include search functionality and user info display

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,28 +1,68 @@
-import { useQuery } from "@apollo/client";
-import { QUERY_USERS } from "../utils/queries";
+import {useState} from "react";
+import {useQuery} from "@apollo/client";
 import {Link} from "react-router-dom";
+import {SEARCH_USERS} from "../utils/queries";
+import styled from 'styled-components';
+
+import {UserInfo} from "../components/UserInfo.jsx";
+import {SearchBar} from "../components/SearchBar.jsx";
+
+
+const HomeContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 85vw;
+  height: 100%;
+`;
+
+const DropdownContainer = styled.div`
+  position: absolute;
+  top: 45px;  // Position right below the search bar
+  width: 17rem;
+  border: 1px solid #ddd;
+  background-color: white;
+  border-radius: 4px;
+  z-index: 1000;  // Ensure dropdown appears above other content
+`;
+
+const SearchResultItem = styled(Link)`
+  display: block;
+  padding: 10px;
+  color: black;
+  text-decoration: none;
+  &:hover {
+    background-color: #f6f6f6;
+  }
+`;
+
 
 export function Home() {
-  const { loading, data } = useQuery(QUERY_USERS);
-  const users = data?.users || [];
+  const [searchTerm, setSearchTerm] = useState('');
+  const { data } = useQuery(SEARCH_USERS, {
+    variables: { searchTerm },
+    skip: !searchTerm
+  });
+
+  const users = data?.searchUsers || [];
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
 
   return (
-    <>
-      {loading ? (
-        <div>Loading...</div>
-      ) : (
-        <div>
-          {users.map((user) => (
-            <div key={user._id}>
-
-              <p>{user.username}</p>
-              <Link to={`/profile/${user._id}`}>
-                <p>{user.firstName}</p>
-              </Link>
-            </div>
+    <HomeContainer>
+      <SearchBar searchTerm={searchTerm} handleSearchChange={handleSearchChange} />
+      <UserInfo />
+      {searchTerm && users.length > 0 && (
+        <DropdownContainer>
+          {users.map(user => (
+            <SearchResultItem to={`/profile/${user._id}`} key={user._id}>
+              {user.username} - {user.email}
+            </SearchResultItem>
           ))}
-        </div>
+        </DropdownContainer>
       )}
-    </>
-  )
+    </HomeContainer>
+  );
 }
