92397438d839f74a9817988bec569172122afe80
didierthomasm <didierthomas.m@gmail.com>
2023-11-21 15:14:53 -0600

    Refactor HelpStyle.js to adjust HelpContainer dimensions and remove margins from SectionTitle and Section

--- client/src/assets/style/Help-Contact/HelpStyle.js
+++ client/src/assets/style/Help-Contact/HelpStyle.js
@@ -4,15 +4,16 @@ export const HelpContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding: 2rem;
+  width: 85vw;
+  height: 100%;
 `;
 
 export const SectionTitle = styled.h2`
-  margin: 2rem 0 1rem;
+  //margin: 2rem 0 1rem;
 `;
 
 export const Section = styled.section`
   max-width: 800px;
   width: 100%;
-  margin-bottom: 2rem;
+  //margin-bottom: 2rem;
 `;
