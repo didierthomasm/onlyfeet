ea59857d4ade631d10d7736dad88b25c031b15df
didierthomasm <didierthomas.m@gmail.com>
2023-11-23 11:37:44 -0600

    Update connection.js and index.html: improve code readability and change page title to "OnlyFeet".

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
-try{
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1/mernshopping');
-console.log("no estas tonto");
-}catch{
-    console.log("si lo estas");
+try {
+  mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1/mernshopping');
+
+
+  console.log('Mongoose is connected');
+
+} catch {
+
+  console.log('error');
 }
 
 module.exports = mongoose.connection;
