d75bdd7d924b0797a0a3b79c6c71bd3fb5ffc2fd
didierthomasm <didierthomas.m@gmail.com>
2023-11-23 12:37:42 -0600

    Update vite.config.js to define a new environment variable for Stripe publishable key.

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig, loadEnv } from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 import react from '@vitejs/plugin-react'
 
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
+export default defineConfig(({mode}) => {
   const env = loadEnv(mode, process.cwd(), 'VITE_APP_');
 
   return {
@@ -19,10 +19,11 @@ export default defineConfig(({ mode }) => {
         }
       }
     },
-   define: {
+    define: {
       'process.env.CLOUDINARY_CLOUD_NAME': JSON.stringify(env.VITE_APP_CLOUDINARY_CLOUD_NAME),
       'process.env.BACKEND_BASEURL': JSON.stringify(env.VITE_APP_BACKEND_BASEURL),
-      'process.env.CLOUDINARY_API_KEY': JSON.stringify(env.VITE_APP_CLOUDINARY_API_KEY)
+      'process.env.CLOUDINARY_API_KEY': JSON.stringify(env.VITE_APP_CLOUDINARY_API_KEY),
+      'process.env.STRIPE_PUBLISHABLE_KEY': JSON.stringify(env.VITE_APP_STRIPE_PUBLISHABLE_KEY),
     }
   }
 })
